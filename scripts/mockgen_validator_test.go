77e243ae88d69ccbd78a443f03e728025cf24fa9
shiranrubin <shiranrubin@microsoft.com>
2022-06-27 10:14:59 +0300

    Add tests

--- scripts/mockgen_validator_test.go
+++ scripts/mockgen_validator_test.go
@@ -6,11 +6,20 @@ import (
 	"testing"
 )
 
-func TestExtractFilesWithMocksSuccessfully(t *testing.T) {
+func TestExtractFilesWithMocksSuccessfullyWithEnvVariable(t *testing.T) {
 	projectGoFiles := map[string]fileData{}
 	mockFiles := map[string]fileData{}
 	os.Setenv("PROJECT_PATH", "../")
 	extractFilesWithMocks(mockFiles, projectGoFiles)
 	assert.Len(t, mockFiles, 1)
 	assert.Len(t, projectGoFiles, 3)
+}
+
+func TestExtractFilesWithMocksSuccessfullyWithDefaultPath(t *testing.T) {
+	projectGoFiles := map[string]fileData{}
+	mockFiles := map[string]fileData{}
+	os.Unsetenv("PROJECT_PATH")
+	extractFilesWithMocks(mockFiles, projectGoFiles)
+	assert.Len(t, mockFiles, 0)
+	assert.Len(t, projectGoFiles, 1)
 }
